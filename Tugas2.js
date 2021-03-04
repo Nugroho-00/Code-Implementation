5209c2f67c1a7b5742ec33d19b4051b723701f6e
Nugroho-00 <snugroho211@gmail.com>
2021-03-04 09:45:07 +0700

    add validation

--- Tugas2.js
+++ Tugas2.js
@@ -1,24 +1,30 @@
-const str = "Saya belajar JavaScript";
+const str = "Saya belajar javascript";
 const arr = [];
 let count = 0;
 let setStr = "";
-for (let i = 0; i <= str.length; i++) {
-  if (str[i] === " " || i >= str.length) {
-    arr[count] = setStr;
-    setStr = "";
-    count++;
-    if (i >= str.length) {
-      let result = "";
-      for (let j = arr.length - 1; j >= 0; j--) {
-        result += arr[j];
-        if (j !== 0) {
-          result += " ";
-        } else {
-          console.log(result);
+if (typeof str === "string") {
+  for (let i = 0; i <= str.length; i++) {
+    if (str[i] === " " || i >= str.length) {
+      arr[count] = setStr;
+      setStr = "";
+      count++;
+      console.log(arr);
+      if (i >= str.length) {
+        let result = "";
+        for (let j = arr.length - 1; j >= 0; j--) {
+          result += arr[j];
+          if (j !== 0) {
+            result += " ";
+          } else {
+            console.log(result);
+          }
         }
       }
+    } else {
+      setStr += str[i];
+      // console.log(setStr);
     }
-  } else {
-    setStr += str[i];
   }
+} else {
+  console.log("Only string can be Input");
 }
